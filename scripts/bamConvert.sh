fec79166bf8a123869b300f29592a34a04c669ab
perllb <per_ludvik.brattaas@med.lu.se>
2017-12-11 13:30:01 +0100

    bamCovert update

--- scripts/bamConvert.sh
+++ scripts/bamConvert.sh
@@ -1,35 +1,48 @@
 #!/bin/sh
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH -A lsens2017-3-2
+#SBATCH -p dell
+#SBATCH -t 05:00:00
+#SBATCH -C mem256GB
+#SBATCH -J bamToBw.%j
+#SBATCH -o bamToBw.%j.out
+#SBATCH -e bamToBw.%j.err
+
 ml foss/2016b
 ml Python/3.5.2
 ml GCC/5.4.0-2.26  OpenMPI/1.10.3
 ml SAMtools/1.4
 
-bamfiles=$1
-echo "The current bamfiles will be processed:"
-echo $bamfiles
-
-for bam in $bamfiles
-
-do echo "Current bamfile: "
+bam=$1
+echo ">> The current bam will be processed:"
 echo $bam
-echo "..."
 
+## Ask user if sorted needed
+sorted=true
+while true; do
+    read -p ">> Is bamfile sorted by coord? (y/n): " yn
+    case $yn in
+	"y" ) echo "> Bam file is sorted."; sorted=true; break;;
+	"n" ) echo "> Bam is not sorted, so sorting needed.."; sorted=false; break;;
+	* ) echo "Please answer \"y\" or \"n\".";;
+    esac
+done
 
 # get sample name
 sample=$(basename $bam .bam)
 echo "Sample: $sample"
-bamsort=$sample.sortedCoord.bam
 
-if [ -f $bamsort ]
+
+if [ sorted==true ]
 then 
-    echo "-- Sorted bamfile exists.."
+    bamsort=$bam
 else
+    bamsort=${sample}.sorted.bam
     echo ">>Sorting bam file.."
     samtools sort -o $bamsort $bam
     echo "-- Bamfile sorted.."
 fi
-
-
 
 ## index bam
 if [ -f $bamsort.bai ] 
@@ -37,18 +50,19 @@ then
     echo "Index file for $bam exists"
 else 
     echo ">>Creating index file for $bam.."
-    samtools index -b $bamsort -o $bamsort.bai
+    samtools index -b $bamsort $bamsort.bai
 fi 
 
-
 # make bigwig
-echo ">>Generating bigwig file from $bam.."
-bamCoverage -b $bamsort -o $sample.bw
-
+if [ -f $sample.bw ]
+then
+    echo "> Bigwig file for $sample exist.. Nothing done"
+else
+    echo ">>Generating bigwig file from $bam.."
+    bamCoverage -b $bamsort -o $sample.bw
+fi
 echo ">>Indexing and bigwig-generation completed for $bam..!"
 echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++ "
 
-done ## processing done!
-
-
+# make bed
 
