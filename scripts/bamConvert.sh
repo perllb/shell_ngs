f2c7aebcd1773960452af047aaa56c23b08780e5
perllb <per_ludvik.brattaas@med.lu.se>
2018-01-11 11:25:24 +0100

    bamconvert getopt and bed opt

--- scripts/bamConvert.sh
+++ scripts/bamConvert.sh
@@ -3,81 +3,99 @@ ml foss/2016b
 ml Python/3.5.2
 ml GCC/5.4.0-2.26  OpenMPI/1.10.3
 ml SAMtools/1.4
+ml ucsc-tools/3.4.3
+ml BEDTools/2.26.0
 
-bam=$1
-sort=$2
 
-### If sort argument is empty, prompt user 
-if [ -z "$2" ]
+usage="$(basename "$0") [-h] [-b <bamfile>] [-s 'y'/'n'] [-bed] -- program to convert bam files to bw and/or bed
+
+where:
+    -h  show this help text
+    -b <bamfile>  name of bamfile to convert
+    -s <y/n>  'y' if sorting needed, 'n' if already sorted!
+    -bed  if set, bedfile will be generated
+
+    "
+
+seed=42
+while getopts ':hb:s:bed:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    b)
+    if [ -r $bam ]
+      then
+        echo "> Bamfile $OPTARG exists and is readable!.. proceeding..";
+        bam=$OPTARG
+    elif [ ! -r $bam ]
+      then
+        echo "> Bamfile $OPTARG does not exists or is not readable!"
+        exit 1
+    fi
+       ;;
+    s) if [ $OPTARG == 'y' || $OPTARG == 'n' ]
+      then sort=$OPTARG
+    else
+      echo "> ERROR: Sort argument invalid!"
+      echo "$usage"
+    fi
+       ;;
+    bed)
+      bed='y'
+      ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "$bam" ]
   then
-  
-    echo "> No 'sort' argument input. If sort needed or not can be specified with 'sort' or 'nosort' arguments"
-  ## Ask user if sorted needed
-  sorted=true
-  while true; do
-      read -p ">> Is bamfile sorted by coord? (y/n): " yn
-      case $yn in
-	  "y" ) echo "> Bam file is sorted."; sorted=true; break;;
-	  "n" ) echo "> Bam is not sorted, so sorting needed.."; sorted=false; break;;
-	  * ) echo "> Please answer \"y\" or \"n\".";;
-      esac
-  done
-
-## If sort argument is nonsort, set $sorted to true
-elif [ "$sort" == 'nosort' ]
-   then
-   sorted=true
-
-## If sort argument is not empty and not sort or nonsort, then prompt user for input
-elif [ "$sort" != 'nosort' ] && [ "$sort" != 'sort' ]
-then
-  echo "> Sort argument unclear.."
-  ## Ask user if sorted needed
-  sorted=true
-  while true; do
-      read -p ">> Is bamfile sorted by coord? (y/n): " yn
-      case $yn in
-	  "y" ) echo "> Bam file is sorted."; sorted=true; break;;
-	  "n" ) echo "> Bam is not sorted, so sorting needed.."; sorted=false; break;;
-	  * ) echo "> Please answer \"y\" or \"n\".";;
-      esac
-  done
-
-## If sort argument is 'sort' then set $sorted to false
-elif [ "$sort" == 'sort' ]
-then 
-    echo "> Bamfile will be sorted.."
-    sorted=false
+    printf "> Error: bamfile not entered..\n\n"
+    echo "$usage" >&2
+    exit 1
+fi
+if [ -z "$sort" ]
+  then
+    printf "> Error: Sort not specified..\n\n"
+    echo "$usage" >&2
+    exit 1
 fi
 
-echo "Sorted: $sorted"
+echo "Sort?: $sort"
 echo '++++++++++++++++++'
 
-#echo $sort
 echo ">> The current bam will be processed:"
 echo $bam
 # get sample name
 sample=$(basename $bam .bam)
 echo "Sample: $sample"
 
-if [ $sorted = true ]
-then 
+if [ $sort = 'n' ]
+then
     bamsort=$bam
 else
     bamsort=${sample}.sorted.bam
-    echo ">>Sorting bam file.."
+    echo ">> Sorting bam file.."
     samtools sort -o $bamsort $bam
     echo "> Bamfile sorted.."
 fi
 
 ## index bam
-if [ -f $bamsort.bai ] 
+if [ -f $bamsort.bai ]
 then
     echo "> Index file for $bam exists"
-else 
+else
     echo ">> Creating index file for $bam.."
     samtools index -b $bamsort $bamsort.bai
-fi 
+fi
 
 # make bigwig
 if [ -f $sample.bw ]
@@ -88,11 +106,19 @@ else
     bamCoverage -b $bamsort -o $sample.bw
 fi
 
+## Bed conversion?
+if [ $bed == 'y']
+  then
+    # make bed from bam
+    if [ -f $sample.bed ]
+    then
+        echo "> Bed file for $sample exist.. Nothing done"
+    else
+        echo "> Generating bed file from $bam.."
+        bedtools bamtobed -i $bam > $sample.bed
+        echo "> Bedfile generated: $sample.bed"
+    fi
+fi
+
 echo ">> Indexing and bigwig-generation completed for $bam..!"
 echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++ "
-
-
-
-
-
-
--- scripts/createUCSC.sh
+++ scripts/createUCSC.sh
@@ -1,9 +1,8 @@
-#!/usr/sh
+#!/usr/bin
 
 file=$1
 
-echo $file; 
-basename=$(basename $file _R1.bw); 
-echo "track type=bigWig name=${basename} visibility=full smoothingWindow=4 autoScale=on description=${basename} bigDataUrl=http://bone.bmc.lu.se/Public/$file" >> UCSC.bw.txt ; 
-echo $basename; 
-
+echo $file;
+basename=$(basename $file _R1.bw);
+echo "track type=bigWig name=${basename} visibility=full smoothingWindow=4 autoScale=on description=${basename} bigDataUrl=http://bone.bmc.lu.se/Public/$file" >> UCSC.bw.txt ;
+echo $basename;
