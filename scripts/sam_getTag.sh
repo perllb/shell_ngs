fe4791f7977df704993e6c268539f289a6cc81ab
perllb <perllbrattaas@gmail.com>
2020-07-21 12:38:59 +0200

    update samTAG to extract seq

--- scripts/sam_getTag.sh
+++ scripts/sam_getTag.sh
@@ -8,24 +8,26 @@
 
 unset samfile
 unset tag
+seq=0
 
-while getopts ":s:t:h" OPTION; do
+while getopts ":i:t:sh" OPTION; do
     case "$OPTION" in
-	s) samfile=$OPTARG ;;
+	i) samfile=$OPTARG ;;
 	t) tag=$OPTARG ;;
+	s) seq=1 ;;
 	h)     
 	    echo "Usage: "
-	    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+	    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"
 	    exit 0 ;;
 	\?) 
 	    echo "Invalid option: $OPTARG" 
 	    echo "Usage: "
-	    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+	    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"
 	    exit 1;;
 	: ) 
 	    echo "Invalid option_ $OPTARG requires an argument" 
 	    echo "Usage: "
-	    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+	    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"
 	    exit 1;;
 	    
     esac
@@ -34,7 +36,7 @@ done
 if [ $OPTIND -eq 1 ]
 then
     echo "Usage: "
-    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"   
     exit 1;
 fi
 
@@ -42,7 +44,7 @@ if [ -z "$samfile" ]
 then
     echo "> Error: No samfile argument"
     echo "Usage: "
-    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"    
     exit 1;
 fi
 
@@ -50,8 +52,18 @@ if [ -z "$tag" ]
 then
     echo "> Error: No sam TAG argument"
     echo "Usage: "
-    echo "   sam_getTag.sh -s <samfile> -t <tag to extract> -h <help>"
+    echo "   sam_getTag.sh -i <samfile> -t <tag to extract> -s <set if mapped sequence should be included> -h <help>"   
     exit 1;
 fi
 
-awk -v tagvar="$tag" '{for (I=1;I<NF;I++) if ($I ~ tagvar) {print $I};}' $samfile | sed 's/'"${tag}"'//g'
+# Check if sequence should be printed
+if [ $seq == 0 ]
+then
+    
+    awk -v tagvar="$tag" '{for (I=1;I<NF;I++) if ($I ~ tagvar) {print $I};}' $samfile | sed 's/'"${tag}"'//g'
+
+else
+
+    awk -v tagvar="$tag" '{for (I=1;I<NF;I++) if ($I ~ tagvar) {print $I "\t" $10 };}' $samfile | sed 's/'"${tag}"'//g'
+
+fi
